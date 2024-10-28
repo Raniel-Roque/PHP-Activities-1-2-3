bb3191014f894e6e939bebf5ea498dd53b741eb0
Raniel-Roque <raniel.roque0509@gmail.com>
2024-10-28 19:22:00 +0800

    Finished UI

--- activity-1.php
+++ activity-1.php
@@ -1,3 +1,18 @@
+<?php 
+    $arrDrinks = array(
+        'Coke' => 15, 
+        'Sprite' => 20, 
+        'Royal' => 20,
+        'Pepsi' => 15,
+        'Mountain Dew' => 20
+    );
+
+    $arrSizes = array(
+        'regular' => 'Regular',
+        'upSize' => 'Up Size (add ₱ 5)',
+        'jumbo' => 'Jumbo (add ₱ 10)'
+    )
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,6 +22,44 @@
     <title>Vendo Machine</title>
 </head>
 <body>
+    <h1>Vendo Machine</h1>
+    <fieldset>
+        <legend>Products: </legend>
+        <?php
+            if(isset($arrDrinks)) {
+                foreach($arrDrinks as $key => $value) {
+                    echo '<input type="checkbox" name="ckbDrink_' . $key . '" id="ckbDrink_' . $key . '">';
+                    echo '<label for="ckbDrink_' . $key . '"> ' . $key . ' - ₱' . $value . '</label><br>';
+                }
+            }
+        ?>
+    </fieldset>
 
+    <fieldset>
+        <legend>Options: </legend>
+        <label for="size">Size: </label>
+
+        <select name="size" id="size">
+            <?php
+                foreach($arrSizes as $key => $value) 
+                    echo '<option value="' . $key .'">' . $value .'</option>';
+            ?>
+        </select>
+
+        <label for="size">Quantity: </label>
+        <input type="number" min="1" value="1">
+        <button type="submit">Check Out</button>
+    </fieldset>
+
+    <hr>
+    <h1>Purchase Summary: </h1>
+    <ul> 
+        <li>
+            piece of name amounting to ₱ price
+        </li>
+    </ul>
+
+    <b>Total Number of Items: </b><br>
+    <b>Total Amount: </b>
 </body>
 </html>
